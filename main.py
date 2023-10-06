406c5688bc828e198b2ff67291fde949d6b338f5
julia1gin <124470751+julia1gin@users.noreply.github.com>
2023-10-06 13:14:16 +0300

    Рабочая версия 06.10. Добавлено меню и субменю. Кнопки есть, но пока не работают.

---
+++ main.py
@@ -0,0 +1,30 @@
+import wx
+class MyFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title)
+
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+        methodMenu = wx.Menu()
+        method2Menu = wx.Menu()
+
+        methodMenu.Append(wx.ID_ANY, "Метод прямоугольников левых частей")
+        methodMenu.Append(wx.ID_ANY, "Метод прямоугольников правых частей")
+        methodMenu.Append(wx.ID_ANY, "Метод парабол")
+        methodMenu.Append(wx.ID_ANY, "Метод трапеций")
+
+        method2Menu.Append(wx.ID_ANY, "Алгоритм №1")
+        method2Menu.Append(wx.ID_ANY, "Алгоритм №2")
+
+        fileMenu.AppendSubMenu(method2Menu, "С постоянным шагом")
+        fileMenu.AppendSubMenu(methodMenu, "С переменным шагом")
+        fileMenu.Append(wx.ID_ANY, 'Кратный интеграл')
+
+        menubar.Append(fileMenu, "Численное интегрирование")
+        self.SetMenuBar(menubar)
+
+app = wx.App()
+frame = MyFrame(None, "Вычислительная математика")
+frame.Centre()
+frame.Show(True)
+app.MainLoop()
