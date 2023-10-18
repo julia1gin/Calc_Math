374294b26330a26c78f391544299a37d5885abab
Nicita Sorokolit <kid04@mail.ru>
2023-10-18 15:32:53 +0300

    fixed var step 2

--- double_integral.py
+++ double_integral.py
@@ -1,29 +1,30 @@
 from math import *
 
-def integral2(f,a,c,b,d,nx,ny):
 
-    def func(fx,q,w):
+def integral2(f, a, c, b, d, nx, ny):
+
+    def func(fx, q, w):
         x = q
         y = w
         return eval(fx)
 
-    hx=(b-a)/nx
-    hy=(d-c)/ny
-    x=a
-    y=c
-    sx=0
-    sy=0
-    while x<= (b-hx):
+    hx = (b-a)/nx
+    hy = (d-c)/ny
+    x = a
+    y = c
+    sx = 0
+    sy = 0
+    while x <= (b-hx):
 
-        while y<=(d-hy):
-            sy=sy+abs(func(f,x,y))
-            y=y+hy
-        Iy=hy*sy
-        sx=sx+Iy
-        x=x+hx
-        sy=0
-        y=c
-    Ix=sx*hx
-    return(Ix)
+        while y <= (d-hy):
+            sy = sy+abs(func(f, x, y))
+            y = y+hy
+        Iy = hy*sy
+        sx = sx+Iy
+        x = x+hx
+        sy = 0
+        y = c
+    Ix = sx*hx
+    return Ix
 
 
