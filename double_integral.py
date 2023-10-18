9fe24c78744c5efdc16655fd454b79d6ef3343e1
julia1gin <124470751+julia1gin@users.noreply.github.com>
2023-10-18 15:24:11 +0300

    Кратный интеграл рабочая версия

--- double_integral.py
+++ double_integral.py
@@ -1,15 +1,11 @@
-import math
-a = float(input('Введите нижний предел : '))
-b = float(input('Введите верхний предел : '))
-c=float(input('Введите нижний предел : '))
-d=float(input('Введите нижний предел : '))
-nx=int(input('число разбиений по х'))
-ny=int(input('число разбиений по y'))
+from math import *
 
-def integr2(nx,ny,a,c,b,d):
+def integral2(f,a,c,b,d,nx,ny):
 
-    def func(x,y):
-        return math.sin(x+y)
+    def func(fx,q,w):
+        x = q
+        y = w
+        return eval(fx)
 
     hx=(b-a)/nx
     hy=(d-c)/ny
@@ -20,7 +16,7 @@ def integr2(nx,ny,a,c,b,d):
     while x<= (b-hx):
 
         while y<=(d-hy):
-            sy=sy+abs(func(x,y))
+            sy=sy+abs(func(f,x,y))
             y=y+hy
         Iy=hy*sy
         sx=sx+Iy
@@ -30,4 +26,4 @@ def integr2(nx,ny,a,c,b,d):
     Ix=sx*hx
     return(Ix)
 
-print(integr2(nx,ny,a,c,b,d))
+
