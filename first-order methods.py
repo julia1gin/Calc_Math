14870279f3d1951cfa7b81146d5af1d59eb134ea
julia1gin <124470751+julia1gin@users.noreply.github.com>
2023-11-14 00:01:49 +0300

    Метод Эйлера для ДУ 1 порядка с графиком

--- first-order methods.py
+++ first-order methods.py
@@ -1,16 +1,30 @@
-# Euler Method for specific function
+'''Euler Method for specific function
 
-# n - количество итераций, h - шаг, (x, y) - начальная точка
-def Euler(n = 10, a = 0, b = 1):
-    h = (b-a)/n
-    x = a
-    y = b
-    while (x <= b-h):
-        y += h * function(x, y)
+n - количество итераций, h - шаг, (x, y) - начальная точка, x0 = a, x_end = b '''
+
+import matplotlib.pyplot as plt
+
+answer = {}
+def Euler(f, n = 10, x0 = 0, x_end = 1, y0 = 1):
+    h = (x_end-x0)/n
+    x = x0
+    y = y0
+    while (x <= x_end-h):
+        y += h * f(x, y)
         x += h
-    return x, y
+        answer[x] = y
+    return answer
 
-def function(x, y):
+def f(x, y):
     return y * (1 - x) # функция первой производной
 
-print(Euler())
+Euler_DE = Euler(f)
+x = Euler_DE.keys()
+y = Euler_DE.values()
+
+fig, ax = plt.subplots()
+plt.title('Метод Эйлера для решения дифференциальных уравнений')
+plt.xlabel('x')
+plt.ylabel('y')
+ax.plot(x, y)
+plt.show()
