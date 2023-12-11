8629c356ee6053f3463abf344817682e5772da0c
julia1gin <124470751+julia1gin@users.noreply.github.com>
2023-12-11 23:45:40 +0300

    Метод половинного деления

---
+++ NonLinearSegmentDivisionMethod.py
@@ -0,0 +1,16 @@
+def f(fx, q):
+    x = q
+    return eval(fx)
+
+
+def SegmentDivision(fx, a, b, E):
+    while abs(b - a) >= E:
+        fa = fx(a)
+        x = (a+b) / 2
+        fx = fx(x)
+        if fa * fx < 0:
+            b = x
+        else:
+            a = x
+    return a, b
+
